b38a68607a0b64d1d84479f8bd6054d6c3e91588
NathanElshaw <121355645+NathanElshaw@users.noreply.github.com>
2023-07-29 21:12:47 -0400

    Added: db connection test

---
+++ src/main/java/ne/Db_Connection.java
@@ -0,0 +1,27 @@
+package ne;
+
+import static com.mongodb.client.model.Filters.eq;
+
+import org.bson.Document;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+public class Db_Connection extends Db_Key {
+    public void main(String[] args) {
+        Db_Key key = new Db_Key();
+        try (MongoClient mongoClient = MongoClients.create(key.main())) {
+            MongoDatabase database = mongoClient.getDatabase("sample_mflix");
+            MongoCollection<Document> collection = database.getCollection("movies");
+            Document doc = collection.find(eq("title", "Back to the Future")).first();
+            if (doc != null) {
+                System.out.println(doc.toJson());
+            } else {
+                System.out.println("No matching documents found.");
+            }
+        }
+        System.out.print(key);
+    }
+}
